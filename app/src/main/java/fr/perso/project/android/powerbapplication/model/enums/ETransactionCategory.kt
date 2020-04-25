41beba491ed511ac07a16be99adae7d006432af3
Thessalène <thessalene.jeanlouis@reseau.eseo.fr>
2020-04-25 17:48:41 +0200

    WIP Pie chart with transaction mock

--- app/src/main/java/fr/perso/project/android/powerbapplication/model/enums/ETransactionCategory.kt
+++ app/src/main/java/fr/perso/project/android/powerbapplication/model/enums/ETransactionCategory.kt
@@ -2,9 +2,16 @@ package fr.perso.project.android.powerbapplication.model.enums
 
 /**
  * Created on 25/04/2020 - 16:32.
- * TODO: Add a class header comment!
+ * Transaction category enum to describe transaction category
  *
  * @author : JEAN-LOUIS Thessalène
  */
-enum class ETransactionCategory {
+enum class ETransactionCategory { //Add icon : String/Int to custom icon
+    UNKNOWN,
+    LOGEMENT,
+    TRANSPORT,
+    DIVERTISSEMENT,
+    SPORT,
+    BANK,
+    AUTRE
 }
