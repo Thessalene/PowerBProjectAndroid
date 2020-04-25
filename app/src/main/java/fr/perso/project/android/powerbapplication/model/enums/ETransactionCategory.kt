ef39ae74133b40558b856bb975ccbfc961c4e8d3
Thessalène <thessalene.jeanlouis@reseau.eseo.fr>
2020-04-25 17:42:49 +0200

    Pie chart with mock transaction

---
+++ app/src/main/java/fr/perso/project/android/powerbapplication/model/enums/ETransactionCategory.kt
@@ -0,0 +1,10 @@
+package fr.perso.project.android.powerbapplication.model.enums
+
+/**
+ * Created on 25/04/2020 - 16:32.
+ * TODO: Add a class header comment!
+ *
+ * @author : JEAN-LOUIS Thessalène
+ */
+enum class ETransactionCategory {
+}
