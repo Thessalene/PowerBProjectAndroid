cd738a5783685d99a9f3632a73a6e29da5ab83c0
Thessalène <thessalene.jeanlouis@reseau.eseo.fr>
2020-04-25 16:24:27 +0200

    WIP Filter account bank list

--- app/src/main/java/fr/perso/project/android/powerbapplication/mocks/MockClass.kt
+++ app/src/main/java/fr/perso/project/android/powerbapplication/mocks/MockClass.kt
@@ -3,7 +3,7 @@ package fr.perso.project.android.powerbproject.mocks
 import fr.perso.project.android.powerbproject.model.Account
 import fr.perso.project.android.powerbproject.model.Bank
 import fr.perso.project.android.powerbproject.model.Transaction
-import fr.perso.project.android.powerbproject.model.enums.EAccountCategory
+import fr.perso.project.android.powerbapplication.model.enums.EAccountCategory
 import fr.perso.project.android.powerbproject.model.enums.EBankName
 import fr.perso.project.android.powerbproject.model.enums.ETransactionType
 import java.util.*
@@ -17,7 +17,7 @@ import java.util.*
 class MockClass {
     companion object{
         @JvmStatic
-        fun mockBankList() : List<Bank>{
+        fun mockBankList() : MutableList<Bank>{
             var bankList = ArrayList<Bank>()
             for(i in 1..3){
                 bankList.add(Bank(EBankName.values()[i], mockAccountList()))
@@ -56,12 +56,16 @@ class MockClass {
             var accountList = ArrayList<Account>()
 
             for(i in 1..2){
-                accountList.add(Account(123+i,"Compte courant ${i}",124*i,EAccountCategory.CURRENT, transactionList))
+                accountList.add(Account(123+i,"Compte courant ${i}",124*i,
+                    EAccountCategory.CURRENT, transactionList))
                 //accountList.add(Account(123+i,"Compte courant ${i}",15*i,EAccountCategory.CURRENT, transactionList2))
-                accountList.add(Account(123+i,"Compte épargne ${i}",1574*i,EAccountCategory.SAVING, transactionList3))
+                accountList.add(Account(123+i,"Compte épargne ${i}",1574*i,
+                    EAccountCategory.SAVING, transactionList3))
                 //accountList.add(Account(123+i,"Compte épargne ${i}",140*i,EAccountCategory.SAVING, transactionList))
-                accountList.add(Account(123+i,"Compte prévisionnel ${i}",1857+i,EAccountCategory.FORECAST, transactionList2))
-                accountList.add(Account(123+i,"Credit ${i}",1124*i,EAccountCategory.CREDIT, transactionList3))
+                accountList.add(Account(123+i,"Compte prévisionnel ${i}",1857+i,
+                    EAccountCategory.FORECAST, transactionList2))
+                accountList.add(Account(123+i,"Credit ${i}",1124*i,
+                    EAccountCategory.CREDIT, transactionList3))
             }
             return accountList
         }
--- app/src/main/java/fr/perso/project/android/powerbapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/fr/perso/project/android/powerbapplication/ui/home/HomeFragment.kt
@@ -5,27 +5,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.LinearLayout.VERTICAL
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.perso.project.android.powerbapplication.R
 import fr.perso.project.android.powerbapplication.adapter.AccountAdapter
-import fr.perso.project.android.powerbapplication.adapter.AccountChoicesAdapter
 import fr.perso.project.android.powerbproject.mocks.MockClass.Companion.mockBankList
-import kotlinx.android.synthetic.main.fragment_home.*
+import fr.perso.project.android.powerbapplication.model.enums.EAccountCategory
+import fr.perso.project.android.powerbproject.model.Bank
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
-
 class HomeFragment : Fragment() {
+
+
+    private var ACCOUNT_FILTER : String = "Tous"
+
+    companion object{
+        lateinit var listToDisplay : MutableList<Bank>
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        listToDisplay = mockBankList()
+        val adapterAccount = AccountAdapter(context!!, listToDisplay)
+
         // Fill recycler view with bankList
         view.recyclerView_accountList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-        view.recyclerView_accountList.adapter = AccountAdapter(context!!, mockBankList())
-        println("MOCK BANK LIST " + mockBankList().toString())
+        view.recyclerView_accountList.adapter = adapterAccount
 
         //Fill spinner
         val stringArray =  resources.getStringArray(R.array.accountChoices)
@@ -35,6 +45,34 @@ class HomeFragment : Fragment() {
                 android.R.layout.simple_spinner_item, stringArray
             )
             view.spinner_account_choice.adapter = adapter
+        }
+
+        view.spinner_account_choice.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) =
+                Toast.makeText(context, "NOTHING CHANGED", Toast.LENGTH_SHORT).show()
+
+            override fun onItemSelected(parent: AdapterView<*>?, v: View?, position: Int, id: Long) {
+                Toast.makeText(context, parent?.selectedItem.toString(), Toast.LENGTH_SHORT).show()
+                ACCOUNT_FILTER = parent?.selectedItem.toString()
+                if(ACCOUNT_FILTER == "Tous"){
+                    listToDisplay = mockBankList()
+                    println("LISTE TOUT : " +listToDisplay.toString())
+                } else {
+                    var i = 0
+                    for(bank in listToDisplay){
+                        bank.accounts = bank.accounts.asSequence().filter{ o -> o.category.name.equals(ACCOUNT_FILTER)}.toList()
+                        println("COMPTES : " + bank.accounts.toString())
+                        adapterAccount.notifyItemChanged(i)
+                        i++
+                    }
+                    listToDisplay.removeAt(0)
+                    println("LISTE AUTRE : " +listToDisplay.toString())
+
+                }
+                view.recyclerView_accountList.adapter=adapterAccount
+                adapterAccount.notifyDataSetChanged()
+                println("ENUM LIST : " + EAccountCategory.values().toString())
+            }
         }
         return view
     }
